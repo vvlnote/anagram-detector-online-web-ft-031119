d01128720c7500ca07438533386783737bfca362
Joy Lin <vvlnote@hotmail.com>
2019-03-25 23:30:10 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,13 +8,14 @@ class Anagram
   
   def match(str_arr)
     sorted_word = @word.split(//).sort_by!{|e| e.downcase}
-    matched_arr = []
-    str_arr.each do |str|
-      temp = str.split(//).sort_by!{|e| e.downcase}
-      if sorted_word == temp
-        matched_arr << str
-      end
-    end
-    matched_arr
+    str_arr.select{|str| str.split(//).sort_by!{|e| e.downcase} == sorted_word}
+    #matched_arr = []
+    #str_arr.each do |str|
+    #  temp = str.split(//).sort_by!{|e| e.downcase}
+    #  if sorted_word == temp
+    #    matched_arr << str
+    #  end
+    #end
+    #matched_arr
   end
 end
