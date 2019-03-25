ab10ff75392a1639729d6bbcf66f8cc2716e41a1
Joy Lin <vvlnote@hotmail.com>
2019-03-25 23:26:18 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(str_arr)
+    sorted_word = @word.split(//).sort_by!{|e| e.downcase}
+    matched_arr = []
+    str_arr.each do |str|
+      temp = str.split(//).sort_by!{|e| e.downcase}
+      if sorted_word == temp
+        matched_arr << str
+      end
+    end
+    matched_arr
+  end
+end
